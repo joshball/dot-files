0bc208456fa90ec571aeacdbf9d70de78f129784
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-12-15 16:53:12 -0800

    Updating babun install script and readme

--- install/babun-post-install.sh
+++ install/babun-post-install.sh
@@ -22,7 +22,7 @@ fancy_echo "Updating babun"
 	successfully pact update
 
 fancy_echo "Updating babun HOME ~/.zshrc"
-	successfully curl https://gist.githubusercontent.com/prabirshrestha/279d8b179d9353fe8694/raw/.zshrc -o ~/.zshrc
+	successfully curl -k https://raw.githubusercontent.com/joshball/dot-files/master/files/babun-home/.zshrc -o ~/.zshrc
 
 
 # fancy_echo "Updating ~/.zshrc"
