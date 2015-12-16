d10897026d4cd3121bc0174efff2fa2e99259395
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-12-16 08:10:47 -0800

    adding more zsh config and .sh

--- files/zsh/rc.sh
+++ files/zsh/rc.sh
@@ -84,7 +84,7 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
-. $DOTFILES_HOME/zsh/aliases # shell aliases
+. $DOTFILES_HOME/zsh/aliases.sh # shell aliases
 #. ~/.dot-files/zsh/git # shell aliases
 
 
